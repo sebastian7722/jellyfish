949630bede937d6f01cb238cd38f868bf37e5806
sebastian7722 <sebasti1001@gmail.com>
2021-09-02 21:17:05 +0200

    adding link to github

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../img/logo.svg';
+import github from '../img/github.svg';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -28,6 +29,14 @@ function Navbar() {
                 <li className="nav-home"><Link className={linkHome} to="/" onClick={() => {setLinkHome("link link-active");setLinkShop("link")}} >Home</Link></li>
                 <li className="nav-shop"><Link className={linkShop} to="/shop" onClick={() => {setLinkShop("link link-active");setLinkHome("link")}} >Shop</Link></li>
             </ul>
+            <div className="nav-right">
+                    <a href="https://github.com/sebastian7722/jellyfish.git" target="_blank" rel="noreferrer" >
+                <div className="btn">
+                        Github
+                        <img src={github} className="github" alt="github icon" />
+                </div>
+                    </a>
+            </div>
         </header>
     );
 }
