4211516021a8cc5d3f5e380a089385afb612d39a
sebastian7722 <sebasti1001@gmail.com>
2021-09-11 12:25:45 +0200

    home card

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,11 +2,13 @@ import art from './img/art.svg'
 
 function Home() {
     return (
-        <section className="article">
+        <main className="article">
+            <section className="shop-card home-card">
                 <img src={art} alt="beautiful landscape art" />
                 <h1 className="title">My React Website</h1>
                 <h3 className="author">&#127912; by sebastian7722</h3>
-        </section>
+            </section>
+        </main>
     );
 }
 
